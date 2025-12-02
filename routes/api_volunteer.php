3a4830b766e627cb73e394a4bd003ca7976fb5fe
Ferdyan Darwis <ferdyan.darwis.fd@gmail.com>
2025-12-02 21:19:17 +0700

    untuk daftar kegiatan volunteer

--- routes/api_volunteer.php
+++ routes/api_volunteer.php
@@ -4,11 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ApiAuthController;
 use App\Http\Controllers\KegiatanController;
+use App\Http\Controllers\PendaftaranController;
 
 
 Route::post('/login', [ApiAuthController::class, 'login']);
 
-// Route yang mewajibkan Login
-Route::middleware(['auth:sanctum'])->group(function () {
   Route::get('/kegiatan', [KegiatanController::class, 'index']);
-});
+  Route::post('/kegiatan/{kegiatanId}/pendaftaran', [PendaftaranController::class, 'store']);
+
