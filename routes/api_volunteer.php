a1449c9c42980f2610bb8b550ca1466c4e49ac7c
Ferdyan Darwis <ferdyan.darwis.fd@gmail.com>
2025-12-12 16:25:38 +0700

    bekend untuk report kegiatan

--- routes/api_volunteer.php
+++ routes/api_volunteer.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ApiAuthController;
 use App\Http\Controllers\KegiatanController;
 use App\Http\Controllers\PendaftaranController;
+use App\Http\Controllers\ReportKegiatanController;
 
 
 
-Route::get('/kegiatan', [KegiatanController::class, 'index']);
-Route::post('/kegiatan/{kegiatanId}/pendaftaran', [PendaftaranController::class, 'store']);
-Route::get('/kegiatan/{kegiatanId}/pendaftaran/status', [PendaftaranController::class, 'status']);
+  Route::get('/kegiatan', [KegiatanController::class, 'index']);
+  Route::post('/kegiatan/{kegiatanId}/report', [ReportKegiatanController::class, 'store']);
+  Route::post('/kegiatan/{kegiatanId}/pendaftaran', [PendaftaranController::class, 'store']);
+  Route::get('/kegiatan/{kegiatanId}/pendaftaran/status', [PendaftaranController::class, 'status']);
